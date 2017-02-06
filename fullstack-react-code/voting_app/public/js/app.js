abd393cedd6728798ae29fab9214bc93716af86d
Eddie Naff <eddienaff@Eddies-MacBook-Pro-8.local>
2017-02-06 06:57:11 -0800

    Refactor w/out constructors using ES7 Property Initializers and Arrow Functions

--- fullstack-react-code/voting_app/public/js/app.js
+++ fullstack-react-code/voting_app/public/js/app.js
@@ -1,23 +1,14 @@
 class ProductList extends React.Component
 {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			products: [],
-		};
-
-		this.handleProductUpVote = this.handleProductUpVote.bind(this);
-	}
-
-
+	state = {
+		products: [],
+	};
 
 	componentDidMount() {
 		this.setState({ products: Seed.products });
 	}
 
-	handleProductUpVote(productId) {
-		// console.log( productId + ' was upvoted.' );
+	handleProductUpVote = (productId) => {
 		const nextProducts = this.state.products.map((product) => {
 			if (product.id === productId) {
 				return Object.assign({}, product,{
@@ -55,13 +46,9 @@ class ProductList extends React.Component
 }
 
 class Product extends React.Component {
-	constructor(props){
-		super(props);
-		this.handleUpVote = this.handleUpVote.bind(this);
-	}
-	handleUpVote() {
-		this.props.onVote(this.props.id);
-	}
+	handleUpVote = () => (
+		this.props.onVote(this.props.id)
+	);
 
 	render() {
 		return (
